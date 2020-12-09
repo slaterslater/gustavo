72f5a0e9926e7e87e213ad050c637de77993d39c
slaterslater <slaterslater@gmail.com>
2020-12-08 21:38:28 -0500

    add output tests and update action_master

--- test_gus.py
+++ test_gus.py
@@ -1,6 +1,20 @@
 import gus
 from src import args, out, urls
 
+STATUS = {"url": "test", "code": 400, "desc": "FAIL"}
+
+
+def test_std_output():
+    assert out.std_format(STATUS) == "[FAIL] [400] test"
+
+
+def test_json_output():
+    assert out.json_format(STATUS) == '{"url": \'test\', "status": 400}'
+
+
+def test_rtf_output():
+    assert out.rtf_format(STATUS) == "\cf3 [400] [FAIL] test"
+
 
 def test_get_status_good():
     status = urls.get_status("https://realpython.com/python-testing/")
